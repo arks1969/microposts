e7c7666aaa487c0a4fa74cf187074317eed23f88
arks1969 <arks1969@gmail.com>
2015-08-09 13:24:04 +0000

    create login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+
+  #get 'users/new'
+
   root to: 'static_pages#home'
+  get 'signup', to: 'users#new'
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   #get 'static_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
